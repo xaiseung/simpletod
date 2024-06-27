8086da3738b65218c5ab350ac6f9f8e7b7fcc834
Jaeseung Lee <xaiseung@airi.kr>
2024-06-27 08:06:23 +0000

    add shell helper

---
+++ 03_01_train_end2end.sh
@@ -0,0 +1,31 @@
+
+
+
+export MODEL=$2
+export MODEL_NAME=$3
+export BATCH=$4
+export OUTPUT=output_new/${MODEL_NAME}
+
+export TRAIN_FILE=./resources/llama3/train.history_belief_action_sys_delex
+export TEST_FILE=./resources/llama3/val.history_belief_action_sys_delex
+
+
+CUDA_VISIBLE_DEVICES=$1 python 03_xai_main.py \
+    --output_dir=$OUTPUT \
+    --model_type=$MODEL \
+    --model_name_or_path=$MODEL_NAME \
+    --do_train \
+    --train_data_file=$TRAIN_FILE \
+    --do_eval \
+    --eval_data_file=$TEST_FILE \
+    --evaluate_during_training \
+    --save_steps 2000 \
+    --logging_steps 1000 \
+    --per_gpu_train_batch_size $BATCH \
+    --num_train_epochs 10 \
+    --block_size 1024 \
+    --gradient_accumulation_steps 8 \
+    --learning_rate 0.0001 \
+    --weight_decay 0.05 \
+    --warmup_steps 1000 \
+    --use_lora
